a42bee6d5cb9d86102e71d9a56130fd601062bac
lushirui1118@gmail.com <lushirui1118@gmail.com@b71b43ed-bea7-fd72-54b8-42704cc2a779>
2013-09-10 03:11:14 +0000

    remove redundant comments some fix in GetPixel.cpp

--- trunk/bmppixel/GetPixel.cpp
+++ trunk/bmppixel/GetPixel.cpp
@@ -11,47 +11,7 @@ BITMAPINFOHEADER * m_pBitmapInfoheader;
 RGBQUAD * m_pRgbQuard;
 BYTE * m_pData;
 BYTE * pDib;
-/*
-bool LoadBmp(const char * filename)
-{
-  ifstream ifs(filename, ios::binary);
-  ifs.seekg(0, ios::end);
-  int size = ifs.tellg();
-  ifs.seekg(0, ios::beg);
-  ifs.read( (char *)&m_BitmapFileHeader, sizeof(BITMAPFILEHEADER) );
 
-  if (m_BitmapFileHeader.bfType != 0x4d42) {
-    throw "文件类型不正确！";
-    return false;
-  }
-  if (size != m_BitmapFileHeader.bfSize) {
-    throw "文件大小描述不正确！";
-    return false;
-  }
-
-  pDib = new BYTE[size - sizeof(BITMAPFILEHEADER)];
-
-  if( !pDib) {
-    throw "out of memory!";
-    return false;
-  }
-  ifs.read( (char *)pDib.size - sizeof(BITMAPFILEHEADER) );
-
-  m_pBitmapInfoHeader = (BITMAPINFOHEADER *)pDib;
-  m_pRgbQuard = (RGBQUARD *)(pDib + sizeof(BITMAPINFOHEADER));
-  int colorTableSize = m_BitmapFileHeader.bfOffBits - sizeof(BITMAPFILEHEADER) - m_pBitmapInfoHeader->biSize;
-  int numberOfColors = GetNumberOfColors();
-
-  if (numberOfColors * sizeof(RGBQUARD) != colorTableSize) {
-    delete[] pDib;
-    pDib = NULL;
-    throw "颜色表大小计算错误！";
-    return false;
-  }
-  m_pData = pDib + sizeof(BITMAPINFOHEADER) + colorTableSize;
-  return true;
-}
-*/
 bool GetPixel(char *pBuf, int x, int y, unsigned short rgb[3])
 {
   assert(pBuf);
@@ -66,13 +26,12 @@ bool GetPixel(char *pBuf, int x, int y, unsigned short rgb[3])
 	  return false;
   }
 
-  RGBQUAD * pRgbQuad = (RGBQUAD *)(pBuf + sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER));
   BYTE * pData = (BYTE *)(pBuf + pBiFileHeader->bfOffBits);
 
   int rwidth = ((pBiInfoHeader->biWidth + 3) >> 2) <<2;
   int ry = pBiInfoHeader->biHeight -1 -y;
   if ( pBiInfoHeader->biBitCount == 8 ) {
-
+	RGBQUAD * pRgbQuad = (RGBQUAD *)(pBuf + sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER));
 	rgb[0] = pRgbQuad[pData[ry*rwidth+x]].rgbBlue;
 	return true;
   }
@@ -82,4 +41,5 @@ bool GetPixel(char *pBuf, int x, int y, unsigned short rgb[3])
 	  rgb[2] = pData[(ry*rwidth+x)*3 + 2];
 	  return true;
   }
+  return false;
 }
